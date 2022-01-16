9e049d6c265e05cd3fe0e8ec15181ef8de925d53
Raymond Wolt <rwolt@pop-os.localdomain>
2022-01-16 16:51:12 -0600

    Library state populates to match uid of signed in user

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-import Header from './components/Header';
-import MainContent from './components/MainContent';
-import { useEffect, useState } from 'react';
-import './App.css';
-import {getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged} from 'firebase/auth';
-import { collection, getFirestore, addDoc, query, where, orderBy, getDocs, onSnapshot } from '@firebase/firestore';
-
-
+import Header from "./components/Header";
+import MainContent from "./components/MainContent";
+import { useEffect, useState } from "react";
+import "./App.css";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+  onAuthStateChanged,
+} from "firebase/auth";
+import {
+  collection,
+  getFirestore,
+  addDoc,
+  query,
+  where,
+  orderBy,
+  getDocs,
+  onSnapshot,
+} from "@firebase/firestore";
 
 function App() {
-
   const [showAddForm, setShowAddForm] = useState(false);
-  const [user, setUser] = useState({name: '', photoURL: ''})
+  const [user, setUser] = useState({ name: "", photoURL: "", uid: "" });
   const [movie, setMovie] = useState({});
   const [movies, setMovies] = useState([]);
 
@@ -19,73 +31,83 @@ function App() {
   }, []);
 
   useEffect(() => {
-
-
-
-    onSnapshot(collection(getFirestore(), 'movies'), (snapshot) => {
-      setMovies((snapshot.docs.map(doc => doc.data())))
-    });
-  }, [])
+    //If the user is authenticated
+    if(!!getAuth().currentUser) {
+      //Get movies for the current user
+      const q = query(
+        collection(getFirestore(), "movies"),
+        where("uid", "==", getAuth().currentUser.uid)
+      );
+      //Set the current state of movies to the snapshot of the movies collection
+      onSnapshot(q, (snapshot) => {
+        setMovies(snapshot.docs.map((doc) => doc.data()));
+      });
+    } else {
+      //If not authenticated, the library is empty
+      setMovies([]);
+    }
+  }, [user]);
 
   const handleForm = () => {
     setShowAddForm(!showAddForm);
-    console.log(showAddForm);
-  }
+  };
 
   //Control values of movie form from App state
   const handleChange = (e) => {
-    const {name} = e.target;
-    setMovie({...movie, [name]: e.target.value})
-  }
+    const { name } = e.target;
+    setMovie({ ...movie, [name]: e.target.value });
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const uid = getAuth().currentUser.uid;
-    setMovie({...movie, user: uid});
     handleForm();
-    try{
-      await addDoc(collection(getFirestore(), 'movies'), {
-        ...movie
+    try {
+      await addDoc(collection(getFirestore(), "movies"), {
+        ...movie, uid: user.uid
       });
+    } catch (error) {
+      console.error("Error writing new message to Firebase Database", error);
     }
-    catch(error) {
-      console.error('Error writing new message to Firebase Database', error);
-    }
-  }
+    setMovie({});
+  };
 
-
-//Signs the user in
+  //Signs the user in
   const signIn = async () => {
     const provider = new GoogleAuthProvider();
     await signInWithPopup(getAuth(), provider);
   };
 
-//Signs the user out
-  const signOutUser = () =>  {
+  //Signs the user out
+  const signOutUser = () => {
     signOut(getAuth());
   };
 
-    //Initiate firebase auth
+  //Initiate firebase auth
   const initFirebaseAuth = () => {
-      //Listen to auth state changes
-      onAuthStateChanged(getAuth(), authStateObserver);
-    }
+    //Listen to auth state changes
+    onAuthStateChanged(getAuth(), authStateObserver);
+  };
 
   const getUserName = () => {
     return getAuth().currentUser.displayName;
-  }
+  };
 
   const getProfilePicUrl = () => {
-    return getAuth().currentUser.photoURL || './images/user.png';
+    return getAuth().currentUser.photoURL || "./images/user.png";
+  };
+
+  const getUserId = () => {
+    return getAuth().currentUser.uid;
   }
 
-const authStateObserver = (user) => {
+  const authStateObserver = (user) => {
     if (user) {
-      setUser({name: getUserName(),  photoURL: getProfilePicUrl()});
+      setUser({ name: getUserName(), photoURL: getProfilePicUrl(), uid: getUserId()});
     } else {
-      setUser({name: '', photoURL: ''});
-    }
-  }
+      setUser({ name: "", photoURL: "", uid: ""});
+    
+    };
+}
 
   return (
     <div className="App">
